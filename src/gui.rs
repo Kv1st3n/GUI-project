8214967137aa5ff56dabe38bccea971bc43ae1fd
Kv1st3n <filip.s.becic@gmail.com>
2025-11-29 23:26:45 +0100

    added text input

--- src/gui.rs
+++ src/gui.rs
@@ -1,19 +1,28 @@
 use eframe::egui;
 
 pub struct MyApp {
-
+    label: String,
 }
 
 impl MyApp {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            label: String::new(),
+        }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Add things to do");
+            ui.heading("Welcome to your to-do list!");
+
+            // Adds a text input
+            ui.horizontal(|ui| {
+                ui.label("Add thing to do: ");
+
+                ui.text_edit_singleline(&mut self.label);
+            });
         });
     }
 }
